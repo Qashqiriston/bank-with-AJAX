952531c57a06bcbe9d0f67eadb7b6b4a819d9df2
Qashqiriston <akobirtojiyev3@gmail.com>
2023-06-24 18:15:46 +0500

    valyutchik ajax bilan tayorlandi

---
+++ js/index.js
@@ -0,0 +1,23 @@
+
+
+const uzs = document.querySelector("#uzs")
+      usd = document.querySelector("#usd")
+
+uzs.addEventListener('input', (e)=>{
+  const request = new XMLHttpRequest()
+
+  request.open("GET", 'json/index.json')
+  request.setRequestHeader('Content-Type' , 'aplacation/json; charset=utf-8')
+  request.send()
+
+  request.addEventListener('load', ()=>{
+    if(request.status === 200){
+      console.log(request.response)
+      const data = JSON.parse(request.response)
+      usd.value = (+uzs.value / data.current.usd).toFixed(2)
+    }else{
+      usd.value = 'somsing wrong'
+    }
+  })
+
+  })
